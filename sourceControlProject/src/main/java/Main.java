e4d502595d7e177bbe217c24727a643080b9201c
Irina Barbos <irina.barbos@accesa.eu>
2019-11-21 11:06:59 +0200

    'test'

---
+++ sourceControlProject/src/main/java/Main.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.net.URL;
+
+public class Main {
+    private static final String versionHash = "null";
+    public static void main(String[] args) {
+        System.out.println("NEW " + versionHash);
+    }
+}
